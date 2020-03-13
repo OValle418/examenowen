d8b1246134bfdb18053385bfc34cdb711dec00b7
OValle418 <owenvalleta@gmail.com>
2020-03-13 16:59:27 -0600

    Solicitando datos del vehiculo

---
+++ estacionamiento.py
@@ -0,0 +1,19 @@
+# -*-coding: utf8-*-
+# Programa: estacionamiento.py
+# Objetivo: Crea una simulacion de un
+#           estacionamiento de vehiculos
+# Autor: Owen Ariel Valle Turcios
+# Fecha: 13/03/2020
+
+class estacionamiento_vehiculo:
+    """
+    Despliega el menu para realizar la insercion de los datos
+    """
+    def __init__(self, placavehiculo,  marca, modelo, tipo):
+        """Inicializa la insercion de los datos del vehiculo"""
+        self.placavehiculo = placavehiculo
+        self.marca = marca
+        self.modelo = modelo
+
+        
+
