e022073df1895aeba7ae262e93bbc566034faf40
OValle418 <owenvalleta@gmail.com>
2020-03-13 18:14:07 -0600

    El estacionamiento representa el menu principal.

--- estacionamiento.py
+++ estacionamiento.py
@@ -6,26 +6,71 @@
 # Fecha: 13/03/2020
 import sys
 import platform
-import os
+import datetime
+import random
 
-class estacionamiento_vehiculo:
+class menu_estacionamiento:
     """
-    Despliega el menu para realizar la insercion de los datos
+    Despliega las opciones del estacionamiento
     """
-    def __init__(self, placavehiculo,  marca, modelo, tipo):
-        """Inicializa la insercion de los datos del vehiculo"""
-        self.placavehiculo = placavehiculo
-        self.marca = marca
-        self.modelo = modelo
-        print("Ingrese los datos del vehiculo:")
+
+    def __init__(self):
+            """ Inicializa las listas de los vehiculos al dia """
+            self.Vehiculo = list() #Lista de los vehiculos
         
-    def limpiar_pantalla(self):
-          os.system("cls")
-    
-    def press_enter(self):
-      """ Realiza una pausa y solicita presionar una tecla """
-      input("\nPresiona Enter para continuar")
+            self.options = {"1":self.ingresovehiculo,
+                            "2":self.totalvehiculos,
+                            "3":self.salidasvehiculos,
+                            "4":self.gananciasdeldia,
+                            "5":self.salir
+                            }
 
-    def parqueo(self):
-        """ Calcula el tiempo que lleva el vehiculo"""
+    def menu_principal(self):
+        """ Despliega el menu de ingreso de vehiculos"""
+        print("""
+             
+                Estacionamiento...
+
+            1:Ingreso de vehiculos
+            2:Total de vehiculos Estacionados
+            3:Salidas
+            4:Ganancias del dia
+            5:Salir
+             """)
+
+    def ingreso_vehiculo(self):
+        placa = input("Ingrese la placa del vehiculo: ")
+        marca = input("Ingrese la marca del vehiculo: ")
+        modelo = input("Ingrese el modelo del vehiculo: ")
+        tipo = input("Ingrese tipo del vehiculo: ")
+        hora_ingreso = input("Ingrese la hora de ingreso del vehiculo: ")
+        estado = input("Estado por defecto True: ")
+        print("El vehiculo se ingreso correctamente")
+        pass
+
+    def totalVehiculos(self):
+ 
+        for vehi in vehiculos:
+            print("placa: {0}\nmarca: '{1}'\nmodelo: {2}\ntipo: {3}"
+                  .format())
+
+
+
+    def salida(self):
+         pass
+
+    def gananciasDia(self):
+         pass
+
+    def salir(self):
+          """ Cierra la aplicación """
+          print("Gracias por utilizar nuestro servicio")
+          sys.exit(0)
+
+    def horas(self):
+      horas_vehiculo=random(1,5)
+      print(horas_vehiculo)
+   if __name__ == "__main__":
+    
+
         
